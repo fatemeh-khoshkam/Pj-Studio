f64d6c452e87aaf171b2c765e3b5a6c558c08473
fatemeh-khoshkam <fa.khoshkam@gmail.com>
2022-05-09 16:02:58 +0430

    update responsive

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/***  mobile menu ***/
+const menu = document.querySelector('#menu');
+const menuOpen = document.querySelector('#nav-open');
+const menuClosed = menu.querySelector('#nav-close');
+const menuOverlay = document.querySelector('.overlay');
+
+
+menuOpen.addEventListener('click', () => {
+    toggleMenu();
+});
+menuClosed.addEventListener('click', () => {
+    toggleMenu();
+});
+menuOverlay.addEventListener('click', () => {
+    toggleMenu();
+});
+function toggleMenu() {
+    menu.classList.toggle('nav__right--open');
+    menuOverlay.classList.toggle('active');
+}
